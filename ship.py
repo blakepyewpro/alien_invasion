937c64136676e00231b777281b56a72512af236d
blakepyewpro <148835335+blakepyewpro@users.noreply.github.com>
2023-12-10 19:30:02 -0500

    Added ship movement and ship speed setting

--- ship.py
+++ ship.py
@@ -8,6 +8,7 @@ class Ship:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         self.image = pygame.image.load('assets/ship.bmp')
         self.rect = self.image.get_rect()
@@ -15,6 +16,19 @@ class Ship:
         #start the ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        self.moving_right = False
+        self.moving_left = False
+        self.x = float(self.rect.x)
+
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """Modify ship's position based on movement flags."""
+        if self.moving_right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left:
+            self.x -= self.settings.ship_speed
+        
+        self.rect.x = self.x
