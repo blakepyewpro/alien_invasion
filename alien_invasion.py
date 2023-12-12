96e49c3c05bf4aba19958c5fb9e0af884d531a64
blakepyewpro <148835335+blakepyewpro@users.noreply.github.com>
2023-12-12 00:32:40 -0500

    Added Bullet class and fullscreen settings.

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,8 +15,14 @@ class AlienInvasion:
         
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, 
-                                               self.settings.screen_height))
+        
+        if self.settings.fullscreen:
+            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+            self.settings.screen_width = self.screen.get_rect().width
+            self.settings.screen_width = self.screen.get_rect().width
+        else:
+            self.screen = pygame.display.set_mode((self.settings.screen_width, 
+                                                   self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
