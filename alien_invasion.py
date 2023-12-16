d9aae7ac18c97a376ce0f5766fe4c36a854c23d5
blakepyewpro <148835335+blakepyewpro@users.noreply.github.com>
2023-12-16 15:03:15 -0500

    Added basic implementation of Alien class

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,12 +5,12 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
-
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
-    def __init__(self) -> None:
+    def __init__(self):
         """Initialize the game and create game resources."""
         pygame.init()
         
@@ -28,6 +28,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         """Start the main gameplay loop."""
@@ -80,12 +83,18 @@ class AlienInvasion:
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
 
+    def _create_fleet(self):
+        """Creates a group of aliens."""
+        alien = Alien(self)
+        self.aliens.add(alien)
+
     def _update_screen(self):
         """Updates images on-screen and flips to the new screen."""
         self.screen.fill(self.settings.bg_color)
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.ship.blitme()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
