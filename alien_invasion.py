52f7538cec025f79b000588650be6dc25aa11fd2
blakepyewpro <148835335+blakepyewpro@users.noreply.github.com>
2023-12-16 15:57:27 -0500

    Update create fleet, added create alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -86,7 +86,24 @@ class AlienInvasion:
     def _create_fleet(self):
         """Creates a group of aliens."""
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+
+            current_x = alien_width
+            current_y += 2 * alien_height
+    
+    def _create_alien(self, x_position, y_position):
+        """Create an alien at the give x and y position"""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
 
     def _update_screen(self):
         """Updates images on-screen and flips to the new screen."""
